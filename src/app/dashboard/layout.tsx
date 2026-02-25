e8047fceae5ddad6063ff7e5cb6b715d24568631
Gabriel Aletor <aletorgabriel513@gmail.com>
2026-02-25 10:40:19 +0000

    make switching between pages faster 0.5secs

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.2 }}
+                transition={{ duration: 0.5 }}
               >
                 {children}
               </motion.div>
